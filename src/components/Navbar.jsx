eb6b8fd80c1361464fb063dd9879458187f790c4
Sodiq578 <sodiqjon@gmail.com>
2024-09-12 10:27:51 +0500

    shop

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,41 +13,46 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="container">
-        <div className="logo">
-          <img src={Logo} alt="Logo" />
-        </div>
-        <div className="menu-icon" onClick={handleShowNavbar}>
-          <div className="hamburger-icon">
-            <div className="line"></div>
-            <div className="line"></div>
-            <div className="line"></div>
+        <div className="nav-wrapper">
+          <div className="logo">
+            <img src={Logo} alt="Logo" />
+          </div>
+          <div className="menu-icon" onClick={handleShowNavbar}>
+            <div className="hamburger-icon">
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+            </div>
+          </div>
+          <div className={`overlay ${showNavbar ? "active" : ""}`} onClick={handleShowNavbar}></div>
+          <div className={`nav-elements ${showNavbar ? "active" : ""}`}>
+            <span className="close-icon" onClick={handleShowNavbar}>&times;</span>
+            <ul>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/blogs">Blogs</NavLink>
+              </li>
+              <li>
+                <NavLink to="/projects">Projects</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about">About</NavLink>
+              </li>
+              <li>
+                <NavLink to="/contact">Contact</NavLink>
+              </li>
+              <li className="browse-categories">
+                <NavLink to="/categories">Browse Categories</NavLink>
+              </li>
+              <li>
+                <NavLink to="/additional-info">Additional Info</NavLink>
+              </li>
+            </ul>
           </div>
         </div>
-        <div className={`nav-elements ${showNavbar ? "active" : ""}`}>
-          <span className="close-icon" onClick={handleShowNavbar}>&times;</span>
-          <ul>
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/blogs">Blogs</NavLink>
-            </li>
-            <li>
-              <NavLink to="/projects">Projects</NavLink>
-            </li>
-            <li>
-              <NavLink to="/about">About</NavLink>
-            </li>
-            <li>
-              <NavLink to="/contact">Contact</NavLink>
-            </li>
-            <li className="browse-categories">
-              <NavLink to="/categories">Browse Categories</NavLink>
-            </li>
-          </ul>
-        </div>
       </div>
-      
     </nav>
   );
 };
