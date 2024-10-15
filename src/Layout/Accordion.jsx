a547f0e90237b4d3fb14975a19441f188c9abae2
Sodiq578 <sodiqjon@gmail.com>
2024-10-15 14:55:01 +0500

    modal qoldi

--- src/Layout/Accordion.jsx
+++ src/Layout/Accordion.jsx
@@ -1,8 +1,14 @@
-import React, { useState } from 'react';
-import './Accordion.css'; // Create this CSS file for styling
+import React, { useState, useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import './Accordion.css';
 
 const Accordion = () => {
   const [activeIndex, setActiveIndex] = useState(null);
+
+  useEffect(() => {
+    AOS.init();
+  }, []);
 
   const toggleAccordion = (index) => {
     setActiveIndex(index === activeIndex ? null : index);
@@ -10,23 +16,25 @@ const Accordion = () => {
 
   const faqItems = [
     {
-      question: "How much does a website cost?",
-      answer: `While we would love to be able to give a definitive, fixed price for a website...`
-    },
-  
-    {
-      question: "Nega aynan bizni tanlashingiz kerak?",
-      answer: `Bizni tanlash sababi...`
-    }, {
-      question: "How much does a website cost?",
-      answer: `While we would love to be able to give a definitive, fixed price for a website...`
+      question: "How much does honey cost?",
+      answer: "The price of honey can vary depending on the specific type of honey you're interested in and the quantity you want to purchase."
     },
     {
-      question: "Nega aynan bizni tanlashingiz kerak?",
-      answer: `Bizni tanlash sababi...`
-    }, 
-    
-    // Qo'shimcha savollar va javoblar
+      question: "Why choose us?",
+      answer: "Choosing us means you're opting for high-quality honey that is 100% pure, natural, and sourced from beekeepers who follow sustainable practices."
+    },
+    {
+      question: "Do you offer delivery?",
+      answer: "Yes, we offer delivery services both locally and internationally, making it convenient for you to enjoy our honey no matter where you are."
+    },
+    {
+      question: "How can I place an order?",
+      answer: "Placing an order is easy on our website. Simply browse through our selection, add items to your cart, and proceed to checkout."
+    },
+    {
+      question: "What types of honey do you sell?",
+      answer: "We offer a wide variety of honey types including wildflower honey, clover honey, acacia honey, and more."
+    }
   ];
 
   return (
@@ -36,7 +44,9 @@ const Accordion = () => {
           <li 
             key={index} 
             className={activeIndex === index ? 'active' : ''} 
-            onClick={() => toggleAccordion(index)} // Bu yerga `onClick` ni li tagiga ham qo'shyapmiz
+            onClick={() => toggleAccordion(index)}
+            data-aos="fade-left" 
+            data-aos-duration="500"
           >
             <h3>{item.question}</h3>
             <div className="answer">
